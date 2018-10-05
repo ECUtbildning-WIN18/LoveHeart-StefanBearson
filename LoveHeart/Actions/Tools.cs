9abd94043c1325f8af070234a2f7c4bdeef87375
Stefan Pettersson <trancedude79@hotmail.com>
2018-10-05 08:14:38 +0200

    Added Tools class

---
+++ LoveHeart/Actions/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoveHeart.Actions
+{
+    class Tools
+    {
+        public static void WriteAt(int fromLeft, int fromTop, string text)
+        {
+            Console.SetCursorPosition(fromLeft,fromTop);
+            Console.Write(text);
+        }
+
+        public static void WriteAt(int fromLeft, int fromTop, string text, ConsoleColor color)
+        {
+            Console.SetCursorPosition(fromLeft, fromTop);
+
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
